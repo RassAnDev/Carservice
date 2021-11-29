ae34fccbc9ec815dd2c69a32b6c450963545418b
Rassandev <Rassandev@gmail.com>
2021-11-29 18:25:51 +0400

    Added a Spring Controller following REST style for class Cars.

---
+++ src/main/java/org/carservice/CarsController.java
@@ -0,0 +1,23 @@
+package org.carservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Create the org.carservice.CarsController class.
+ * This is a Spring Controller following REST style.
+ */
+
+@RestController
+public class CarsController {
+    @Autowired
+    private CarsService service;
+
+    @GetMapping("/cars")
+    public List<Cars> list() {
+        return service.listAll();
+    }
+}
