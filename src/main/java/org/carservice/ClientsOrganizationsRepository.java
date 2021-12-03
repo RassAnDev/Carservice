6f3d933beeff2d2c1c0a2e7d39f2eebc0b0dc3cd
Rassandev <Rassandev@gmail.com>
2021-12-03 18:10:47 +0400

    Added a Spring Controller following REST style for class ClientsOrganizations.

---
+++ src/main/java/org/carservice/ClientsOrganizationsRepository.java
@@ -0,0 +1,10 @@
+package org.carservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Create the ClientsOrganizationsRepository for take advantages of Spring Data JPA.
+ */
+
+public interface ClientsOrganizationsRepository extends JpaRepository<ClientsOrganizations, Integer> {
+}
