5de33915efe69d7c8f3ccafc4868de97bff6c730
Rassandev <Rassandev@gmail.com>
2021-12-07 15:36:50 +0400

    Added a Spring Controller following REST style for class Users.

---
+++ src/main/java/org/carservice/Users.java
@@ -0,0 +1,54 @@
+package org.carservice;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Create the Domain Model Class(org.carservice.Users) to map with the "users" table in the database.
+ */
+
+@Entity
+public class Users {
+    private Integer userId;
+    private String username;
+    private String password;
+
+    public Users() {
+    }
+
+    public Users(Integer userId, String username, String password) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
